a54c28e4fc2f31aa8eedef618cf6ebb1517ba00f
ChanHyung-Lee <max.lee@goorm.io>
2023-06-06 00:08:52 +0900

    feat(main): 메인 페이지 퍼블리싱

--- hmr/src/App.tsx
+++ hmr/src/App.tsx
@@ -4,11 +4,17 @@ import React from 'react';
 import Logo from './Logo';
 import './App.css';
 import Header from './components/Header';
-
+import StartTemplateButton from './components/StartTemplateButton';
+import './goormstrap.css';
+import SearchInput from './components/SearchInput';
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="startTemplateButtonContainer">
+        <StartTemplateButton />
+      </div>
+      <SearchInput />
     </div>
   );
 }
