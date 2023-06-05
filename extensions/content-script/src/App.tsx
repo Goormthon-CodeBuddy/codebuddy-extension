cd7ebcd3210cb0e0db0071fd38b2b6a21b06775a
goorm <goorm@SummerBae.local>
2023-06-05 21:03:58 +0900

    feat(notification): background.ts 설정

--- extensions/content-script/src/App.tsx
+++ extensions/content-script/src/App.tsx
@@ -1,16 +1,27 @@
 /// <reference types="chrome" />
 /// <reference types="vite-plugin-svgr/client" />
 
-import Logo from "./Logo";
-import "./App.css";
+import React from 'react';
+import Logo from './Logo';
+import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
+  const onClickHandler = () => {
+    chrome.runtime.sendMessage({ time: '1' }, function (response) {
+      console.log(response);
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <Logo className="App-logo" id="App-logo" title="React logo" />
-        <p>Hello, World!</p>
-        <p>I'm a Chrome Extension Content Script!</p>
+        <p>Hello</p>
+        <p>I'm a Chrome Extension Content</p>
+        <Button variant="primary" onClick={onClickHandler}>
+          Primary
+        </Button>
       </header>
     </div>
   );
