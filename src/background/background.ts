480c0c6e3a73eb3f559c04a8a6ac6a274ed335e2
RookieAND <gwangin1999@naver.com>
2023-06-05 18:50:52 +0900

    fix: Content-Script 가 정상적으로 실행되지 않았던 문제 수정

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,6 +1,21 @@
-function polling() {
-  // console.log("polling");
-  setTimeout(polling, 1000 * 30);
-}
+// const color = '#3aa757';
 
-polling();
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.storage.sync.set({ color });
+//   console.log('Default background color set to %cgreen', `color: ${color}`);
+// });
+
+chrome.runtime.onInstalled.addListener(({ reason }) => {
+  if (reason === chrome.runtime.OnInstalledReason.INSTALL) {
+    showReadme();
+  }
+});
+
+chrome.action.onClicked.addListener(() => {
+  showReadme();
+});
+
+function showReadme() {
+  const url = chrome.runtime.getURL('readme.html');
+  chrome.tabs.create({ url });
+}
